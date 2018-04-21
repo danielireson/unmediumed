e69e003c859672a8b4272c2f138dec84c446b85a
Daniel Ireson <daniel.ireson@zestia.com>
2018-04-21 14:39:44 +0100

    rename Response as ApiGatewayResponse

--- src/main/scala/unmediumed/ApiGatewayResponse.scala
+++ src/main/scala/unmediumed/ApiGatewayResponse.scala
@@ -5,19 +5,19 @@ import java.{util => ju}
 import scala.beans.BeanProperty
 import scala.collection.JavaConverters
 
-case class Response(
+case class ApiGatewayResponse(
     @BeanProperty statusCode: Integer,
     @BeanProperty body: String,
     @BeanProperty headers: ju.Map[String, Object],
     @BeanProperty base64Encoded: Boolean)
 
-object Response {
+object ApiGatewayResponse {
   def apply(
       statusCode: Integer,
       body: String,
       headers: Map[String, Object] = Map(),
-      base64Encoded: Boolean = true): Response = {
+      base64Encoded: Boolean = true): ApiGatewayResponse = {
 
-    new Response(statusCode, body, JavaConverters.mapAsJavaMap[String, Object](headers), base64Encoded)
+    new ApiGatewayResponse(statusCode, body, JavaConverters.mapAsJavaMap[String, Object](headers), base64Encoded)
   }
 }
--- src/main/scala/unmediumed/Handler.scala
+++ src/main/scala/unmediumed/Handler.scala
@@ -2,10 +2,10 @@ package unmediumed
 
 import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
 
-class Handler extends RequestHandler[ApiGatewayRequest, Response] {
-  def handleRequest(input: ApiGatewayRequest, context: Context): Response = {
+class Handler extends RequestHandler[ApiGatewayRequest, ApiGatewayResponse] {
+  def handleRequest(input: ApiGatewayRequest, context: Context): ApiGatewayResponse = {
     val request = input.toRequest
-    Response(200, "Go Serverless v1.0! Your function executed successfully!", Map(
+    ApiGatewayResponse(200, "Go Serverless v1.0! Your function executed successfully!", Map(
       "path" -> request.path,
       "headers" -> request.headers,
       "pathParameters" -> request.pathParameters

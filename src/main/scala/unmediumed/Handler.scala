114596751eb1e3268fc408c6266352c7b274f17f
Daniel Ireson <daniel.ireson@zestia.com>
2018-04-21 14:43:55 +0100

    use wildcard java converters

--- src/main/scala/unmediumed/Handler.scala
+++ src/main/scala/unmediumed/Handler.scala
@@ -5,6 +5,9 @@ import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
 class Handler extends RequestHandler[ApiGatewayRequest, ApiGatewayResponse] {
   def handleRequest(input: ApiGatewayRequest, context: Context): ApiGatewayResponse = {
     val request = input.toRequest
+
+
+
     ApiGatewayResponse(200, "Go Serverless v1.0! Your function executed successfully!", Map(
       "path" -> request.path,
       "headers" -> request.headers,

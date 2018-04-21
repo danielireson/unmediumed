7ae99ed8a5e2ede98abd9ab2ae318dbe5596fcf6
Daniel Ireson <daniel.ireson@zestia.com>
2018-04-21 11:39:05 +0100

    rename classes to be more generic

--- src/main/scala/unmediumed/Handler.scala
+++ src/main/scala/unmediumed/Handler.scala
@@ -3,10 +3,10 @@ package unmediumed
 import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
 import scala.collection.JavaConverters
 
-class ApiGatewayHandler extends RequestHandler[Request, ApiGatewayResponse] {
-  def handleRequest(input: Request, context: Context): ApiGatewayResponse = {
+class Handler extends RequestHandler[Request, Response] {
+  def handleRequest(input: Request, context: Context): Response = {
     val headers = Map("x-custom-response-header" -> "my custom response header value")
-    ApiGatewayResponse(200, "Go Serverless v1.0! Your function executed successfully!",
+    Response(200, "Go Serverless v1.0! Your function executed successfully!",
       JavaConverters.mapAsJavaMap[String, Object](headers),
       true)
   }
--- src/main/scala/unmediumed/Response.scala
+++ src/main/scala/unmediumed/Response.scala
@@ -2,4 +2,5 @@ package unmediumed
 
 import scala.beans.BeanProperty
 
-case class Response(@BeanProperty message: String, @BeanProperty request: Request)
+case class Response(@BeanProperty statusCode: Integer, @BeanProperty body: String,
+    @BeanProperty headers: java.util.Map[String, Object], @BeanProperty base64Encoded: Boolean = false)

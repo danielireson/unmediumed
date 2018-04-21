f85ec008fb426f9bedb74d06f6e3ff072fbf2b00
Daniel Ireson <daniel.ireson@zestia.com>
2018-04-21 14:53:01 +0100

    wrap Response in ApiGatewayResponse

--- src/main/scala/unmediumed/ApiGatewayResponse.scala
+++ src/main/scala/unmediumed/ApiGatewayResponse.scala
@@ -3,21 +3,9 @@ package unmediumed
 import java.{util => ju}
 
 import scala.beans.BeanProperty
-import scala.collection.JavaConverters
 
-case class ApiGatewayResponse(
-    @BeanProperty statusCode: Integer,
-    @BeanProperty body: String,
-    @BeanProperty headers: ju.Map[String, Object],
-    @BeanProperty base64Encoded: Boolean)
-
-object ApiGatewayResponse {
-  def apply(
-      statusCode: Integer,
-      body: String,
-      headers: Map[String, Object] = Map(),
-      base64Encoded: Boolean = true): ApiGatewayResponse = {
-
-    new ApiGatewayResponse(statusCode, body, JavaConverters.mapAsJavaMap[String, Object](headers), base64Encoded)
-  }
-}
+class ApiGatewayResponse(
+    @BeanProperty val statusCode: Integer,
+    @BeanProperty val body: String,
+    @BeanProperty val headers: ju.Map[String, Object],
+    @BeanProperty val base64Encoded: Boolean)
---
+++ src/main/scala/unmediumed/Response.scala
@@ -0,0 +1,18 @@
+package unmediumed
+
+import scala.collection.JavaConverters._
+
+case class Response(
+  statusCode: Integer,
+  body: String,
+  headers: Map[String, Object] = Map(),
+  base64Encoded: Boolean = true) {
+
+  def toApiGatewayResponse: ApiGatewayResponse =
+    new ApiGatewayResponse(
+      statusCode,
+      body,
+      headers.asJava,
+      base64Encoded
+    )
+}

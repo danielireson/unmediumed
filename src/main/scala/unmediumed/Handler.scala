e5532a65367feaf72e627860c3f6dbad48cbd4ac
Daniel Ireson <daniel.ireson@zestia.com>
2018-04-21 14:54:27 +0100

    rename 'input' as 'apiGatewayRequest'

--- src/main/scala/unmediumed/Handler.scala
+++ src/main/scala/unmediumed/Handler.scala
@@ -3,8 +3,8 @@ package unmediumed
 import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
 
 class Handler extends RequestHandler[ApiGatewayRequest, ApiGatewayResponse] {
-  def handleRequest(input: ApiGatewayRequest, context: Context): ApiGatewayResponse = {
-    val request = input.toRequest
+  def handleRequest(apiGatewayRequest: ApiGatewayRequest, context: Context): ApiGatewayResponse = {
+    val request = apiGatewayRequest.toRequest
     val response = Response(200, "Go Serverless v1.0! Your function executed successfully!", Map(
       "path" -> request.path,
       "headers" -> request.headers,

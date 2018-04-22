d7c699c353ada4818287c94d28bed6cbe5036387
Daniel Ireson <daniel.ireson@zestia.com>
2018-04-22 19:06:53 +0100

    throw exception on null input

--- src/main/scala/unmediumed/Handler.scala
+++ src/main/scala/unmediumed/Handler.scala
@@ -4,7 +4,11 @@ import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
 
 class Handler extends RequestHandler[Input, Output] {
   def handleRequest(input: Input, context: Context): Output = {
-    val request = input.toRequest
+    val request = Option(input) match {
+      case Some(r: Input) => r.toRequest
+      case None => throw new IllegalArgumentException("Request input is invalid")
+    }
+
     val response = Response(200, "Go Serverless v1.0! Your function executed successfully!", Map(
       "x-extra-header" -> "abc"
     ))

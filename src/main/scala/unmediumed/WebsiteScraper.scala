5e6935fae289448e8ecc1e26fa50ace105d3dd78
Daniel Ireson <daniel.ireson@zestia.com>
2018-04-24 20:54:01 +0100

    use regex to test valid url

--- src/main/scala/unmediumed/WebsiteScraper.scala
+++ src/main/scala/unmediumed/WebsiteScraper.scala
@@ -3,27 +3,31 @@ package unmediumed
 import java.io.InputStream
 import java.net.{HttpURLConnection, URL}
 
+import scala.util.matching.Regex
+
 class WebsiteScraper {
   val timeout: Int = 5000
+  val ValidWebsite: Regex = "(a-zA-Z0-9)+".r
 
   def scrape(url: String): String = {
+    url match {
+      case ValidWebsite(u) => scrapeWebsiteWithBufferedReader(u)
+      case _ => throw new IllegalArgumentException("Creating input stream from invalid URL")
+    }
+  }
+
+  private def scrapeWebsiteWithBufferedReader(url: String): String = {
     val inputStream = createInputStream(url)
     val html = io.Source.fromInputStream(inputStream).mkString
     if (inputStream != null) inputStream.close()
-
     html
   }
 
   private def createInputStream(url: String): InputStream = {
-    if (url == null || url == "") {
-      throw new IllegalArgumentException("Creating input stream from invalid URL")
-    }
-
     val connection = new URL(url).openConnection.asInstanceOf[HttpURLConnection]
     connection.setRequestMethod("GET")
     connection.setConnectTimeout(timeout)
     connection.setReadTimeout(timeout)
-
     connection.getInputStream
   }
 }

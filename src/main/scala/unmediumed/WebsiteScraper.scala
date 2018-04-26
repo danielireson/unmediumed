aa1c84a6c4d7c3b601091fca8de4d19297bea6ba
Daniel Ireson <daniel.ireson@zestia.com>
2018-04-26 20:11:00 +0100

    add base UnitSpec trait

--- src/main/scala/unmediumed/WebsiteScraper.scala
+++ src/main/scala/unmediumed/WebsiteScraper.scala
@@ -5,29 +5,58 @@ import java.net.{HttpURLConnection, URL}
 
 import scala.util.matching.Regex
 
-class WebsiteScraper {
-  val timeout: Int = 5000
-  val ValidWebsite: Regex = "(a-zA-Z0-9)+".r
+trait WebsiteScraperComponentLocal {
+  def websiteScraper: WebsiteScraperLocal
 
-  def scrape(url: String): String = {
-    url match {
-      case ValidWebsite(u) => scrapeWebsiteWithBufferedReader(u)
-      case _ => throw new IllegalArgumentException("Creating input stream from invalid URL")
+  trait WebsiteScraperLocal {
+    def scrape(url: String): String
+  }
+}
+
+trait WebsiteScraperComponent extends WebsiteScraperComponentLocal {
+  def websiteScraper = new WebsiteScraperComponent
+
+  class WebsiteScraperComponent extends WebsiteScraperLocal {
+    val timeout: Int = 5000
+    val ValidWebsite: Regex = "(a-zA-Z0-9)+".r
+
+    def scrape(url: String): String = {
+      url match {
+        case ValidWebsite(u) => scrapeWebsiteWithBufferedReader(u)
+        case _ => throw new IllegalArgumentException("Creating input stream from invalid URL")
+      }
     }
-  }
 
-  private def scrapeWebsiteWithBufferedReader(url: String): String = {
-    val inputStream = createInputStream(url)
-    val html = io.Source.fromInputStream(inputStream).mkString
-    if (inputStream != null) inputStream.close()
-    html
-  }
+    private def scrapeWebsiteWithBufferedReader(url: String): String = {
 
-  private def createInputStream(url: String): InputStream = {
-    val connection = new URL(url).openConnection.asInstanceOf[HttpURLConnection]
-    connection.setRequestMethod("GET")
-    connection.setConnectTimeout(timeout)
-    connection.setReadTimeout(timeout)
-    connection.getInputStream
+      val inputStream = try {
+        createInputStream(url)
+      }
+
+      try {
+        io.Source.fromInputStream(inputStream).mkString
+      } catch {
+
+      } finally  {
+
+      }
+
+
+      val inputStream = try {
+        createInputStream(url)
+        val html = io.Source.fromInputStream(inputStream).mkString
+        html
+      } finally {
+        if (inputStream != null) inputStream.close()
+      }
+    }
+
+    private def createInputStream(url: String): InputStream = {
+      val connection = new URL(url).openConnection.asInstanceOf[HttpURLConnection]
+      connection.setRequestMethod("GET")
+      connection.setConnectTimeout(timeout)
+      connection.setReadTimeout(timeout)
+      connection.getInputStream
+    }
   }
 }
---
+++ src/test/scala/unmediumed/UnitSpec.scala
@@ -0,0 +1,6 @@
+package unmediumed
+
+import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.mockito.MockitoSugar
+
+trait UnitSpec extends FlatSpec with Matchers with MockitoSugar

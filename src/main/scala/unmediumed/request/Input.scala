330bc4fbf078a8de88bbe983b79a5240e680b4c1
Daniel Ireson <daniel.ireson@zestia.com>
2018-05-06 21:40:53 +0100

    fix input/output formatting for aws lambda

--- src/main/scala/unmediumed/request/Input.scala
+++ src/main/scala/unmediumed/request/Input.scala
@@ -6,22 +6,22 @@ import scala.beans.BeanProperty
 import scala.collection.JavaConverters._
 
 class Input(
-    @BeanProperty val httpMethod: String,
-    @BeanProperty val path: String,
-    @BeanProperty val body: String,
-    @BeanProperty val headers: ju.Map[String, Object],
-    @BeanProperty val pathParameters: ju.Map[String, Object],
-    @BeanProperty val queryStringParameters: ju.Map[String, Object]) {
+    @BeanProperty var httpMethod: String,
+    @BeanProperty var path: String,
+    @BeanProperty var body: String,
+    @BeanProperty var headers: ju.Map[String, Object],
+    @BeanProperty var pathParameters: ju.Map[String, Object],
+    @BeanProperty var queryStringParameters: ju.Map[String, Object]) {
 
-  def this() = this("", "", "", new ju.HashMap(), new ju.HashMap(), new ju.HashMap())
+  def this() = this(null, null, null, null, null, null)
 
   def toRequest: Request =
     Request(
-      httpMethod,
-      path,
-      body,
-      headers.asScala.toMap,
-      pathParameters.asScala.toMap,
-      queryStringParameters.asScala.toMap
+      Option(httpMethod).getOrElse(""),
+      Option(path).getOrElse(""),
+      Option(body).getOrElse(""),
+      Option(headers).map(_.asScala.toMap).getOrElse(Map()),
+      Option(pathParameters).map(_.asScala.toMap).getOrElse(Map()),
+      Option(queryStringParameters).map(_.asScala.toMap).getOrElse(Map())
     )
 }
--- src/main/scala/unmediumed/response/Output.scala
+++ src/main/scala/unmediumed/response/Output.scala
@@ -5,7 +5,7 @@ import java.{util => ju}
 import scala.beans.BeanProperty
 
 class Output(
-    @BeanProperty val statusCode: Integer,
-    @BeanProperty val body: String,
-    @BeanProperty val headers: ju.Map[String, String],
-    @BeanProperty val base64Encoded: Boolean)
+    @BeanProperty var statusCode: Integer,
+    @BeanProperty var body: String,
+    @BeanProperty var headers: ju.Map[String, String],
+    @BeanProperty var base64Encoded: Boolean)

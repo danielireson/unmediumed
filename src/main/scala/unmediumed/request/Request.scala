8f1401bba8455cabfb899901d0ea1ecf1d9a9f11
Daniel Ireson <daniel.ireson@zestia.com>
2018-05-07 14:19:56 +0100

    remove unused toInput parameter

--- src/main/scala/unmediumed/request/Request.scala
+++ src/main/scala/unmediumed/request/Request.scala
@@ -1,22 +1,9 @@
 package unmediumed.request
 
-import scala.collection.JavaConverters._
-
 sealed case class Request(
     httpMethod: String,
     path: String,
     body: String = "",
     headers: Map[String, Object] = Map(),
     pathParameters: Map[String, Object] = Map(),
-    queryStringParameters: Map[String, Object] = Map()) {
-
-  def toInput: Input =
-    new Input(
-      httpMethod,
-      path,
-      body,
-      headers.asJava,
-      pathParameters.asJava,
-      queryStringParameters.asJava
-    )
-}
+    queryStringParameters: Map[String, Object] = Map())

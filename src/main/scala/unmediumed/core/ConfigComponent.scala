4426dd1e5778eaf6f0c14ab02a34e2bf12e9041f
Daniel Ireson <daniel.ireson@zestia.com>
2018-06-02 20:20:03 +0100

    update Config to split comma separated params

--- src/main/scala/unmediumed/core/ConfigComponent.scala
+++ src/main/scala/unmediumed/core/ConfigComponent.scala
@@ -3,10 +3,22 @@ package unmediumed.core
 trait ConfigComponent {
   val config: Config
 
-  class Config(store: Map[String, Any]) {
-    def get(key: String): Any = {
-      store.get(key).orElse {
-        throw new IllegalArgumentException("Invalid configuration key")
+  class Config(params: String = "") {
+    val store: Map[String, String] = loadStore(params)
+
+    def get(key: String): String = {
+      store.getOrElse(key, throw new IllegalArgumentException("Invalid configuration key"))
+    }
+
+    private def loadStore(params: String): Map[String, String] = {
+      params.split(",").foldLeft(Map.empty[String, String]) {(params, param) =>
+        val components = param.split("=")
+
+        if (components.length == 2) {
+          Map(components(0) -> components(1))
+        } else {
+          params
+        }
       }
     }
   }
--- src/test/scala/unmediumed/core/ConfigUnitSpec.scala
+++ src/test/scala/unmediumed/core/ConfigUnitSpec.scala
@@ -5,8 +5,7 @@ import unmediumed.UnitSpec
 class ConfigUnitSpec extends UnitSpec {
   "Config" should "throw an IllegalArgumentException when looking up a key with no value" in {
     // given
-    val store: Map[String, Any] = Map()
-    val testSubject = new Config(store)
+    val testSubject = new Config("")
     val key: String = "invalid"
 
     // when
@@ -16,5 +15,29 @@ class ConfigUnitSpec extends UnitSpec {
 
     // then
     error.getMessage shouldBe "Invalid configuration key"
+  }
+
+  it should "load a store with a single parameter" in {
+    // given
+    val testSubject = new Config("a=1")
+    val key: String = "a"
+
+    // when
+    val value: String = testSubject.get(key)
+
+    // then
+    value shouldBe "1"
+  }
+
+  it should "load a store with multiple parameters" in {
+    // given
+    val testSubject = new Config("a=1,b=2")
+    val key: String = "b"
+
+    // when
+    val value: String = testSubject.get(key)
+
+    // then
+    value shouldBe "2"
   }
 }

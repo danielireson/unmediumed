6ab4439807e26ec4204bdf60f50b8890c645f4ac
Daniel Ireson <daniel.ireson@zestia.com>
2018-06-11 22:15:28 +0100

    convert TemplateBuilderComponent to TemplateBuilder

---
+++ src/main/scala/unmediumed/response/TemplateBuilder.scala
@@ -0,0 +1,12 @@
+package unmediumed.response
+
+class TemplateBuilder(baseTemplate: String) {
+  def build(params: Map[String, String] = Map()): String = {
+    Option(baseTemplate) match {
+      case Some(bt) => params.keys.foldLeft(bt)((html, param) => {
+        html.replace(s"{{$param}}", params(param))
+      })
+      case _ => throw new IllegalArgumentException("Invalid base template")
+    }
+  }
+}

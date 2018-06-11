94f6a73e66477fdb2ffa3d6bd02eb3a99929b8d1
Daniel Ireson <daniel.ireson@zestia.com>
2018-06-11 22:14:32 +0100

    delete ComponentRegistry and TestComponentRegistry

--- src/test/scala/unmediumed/UnitSpec.scala
+++ src/test/scala/unmediumed/UnitSpec.scala
@@ -4,4 +4,4 @@ import org.scalatest.{FlatSpec, Matchers}
 import org.scalatest.mockito.MockitoSugar
 import unmediumed.core.TestComponentRegistry
 
-trait UnitSpec extends FlatSpec with Matchers with MockitoSugar with TestComponentRegistry
+trait UnitSpec extends FlatSpec with Matchers with MockitoSugar

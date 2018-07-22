c90a588a58afa577d534bd2982d27b10dcecaad0
Daniel Ireson <daniel.ireson@zestia.com>
2018-07-22 16:00:01 +0100

    remove override property from markdown elements

--- api/src/main/scala/unmediumed/parse/MarkdownElement.scala
+++ api/src/main/scala/unmediumed/parse/MarkdownElement.scala
@@ -5,35 +5,35 @@ trait MarkdownElement {
 }
 
 sealed case class HeaderMarkdownElement(size: Int, content: String) extends MarkdownElement {
-  override def markdown: String = "#" * size + " " + content
+  def markdown: String = "#" * size + " " + content
 }
 
 sealed case class ParagraphMarkdownElement(content: String) extends MarkdownElement {
-  override def markdown: String = content
+  def markdown: String = content
 }
 
 sealed case class ImageMarkdownElement(src: String) extends MarkdownElement {
-  override def markdown: String = "![](" + src + ")"
+  def markdown: String = "![](" + src + ")"
 }
 
 sealed case class UnorderedMarkdownElement(items: Seq[String]) extends MarkdownElement {
-  override def markdown: String =
+  def markdown: String =
     items.map { item =>
       "* " + item
     }.mkString("\n")
 }
 
 sealed case class OrderedMarkdownElement(items: Seq[String]) extends MarkdownElement {
-  override def markdown: String =
+  def markdown: String =
     items.zipWithIndex.map { case (item, i) =>
       (i + 1) + ". " + item
     }.mkString("\n")
 }
 
 sealed case class BlockquoteMarkdownElement(content: String) extends MarkdownElement {
-  override def markdown: String = "> " + content
+  def markdown: String = "> " + content
 }
 
 sealed case class CodeblockMarkdownElement(content: String) extends MarkdownElement {
-  override def markdown: String = "```\n" + content + "\n```"
+  def markdown: String = "```\n" + content + "\n```"
 }

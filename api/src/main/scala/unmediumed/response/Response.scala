49d006fcbf01ccf006d9b6faf3d5c4a7eae218b1
Daniel Ireson <daniel.ireson@zestia.com>
2018-07-22 15:42:20 +0100

    add content type by default to responses

--- api/src/main/scala/unmediumed/response/Response.scala
+++ api/src/main/scala/unmediumed/response/Response.scala
@@ -5,20 +5,19 @@ import scala.collection.JavaConverters._
 sealed case class Response(
     statusCode: Integer,
     body: String,
-    headers: Map[String, String] = Map(),
+    customHeaders: Map[String, String] = Map(),
     base64Encoded: Boolean = true) {
 
-  def toOutput: Output = new Output(statusCode, body, headers.asJava, base64Encoded)
+  val commonHeaders: Map[String, String] = Map("content-type" -> "text/markdown")
+  val responseHeaders: Map[String, String] = commonHeaders ++ customHeaders
+
+  def toOutput: Output = new Output(statusCode, body, responseHeaders.asJava, base64Encoded)
 }
 
-class OkResponse(body: String)
-  extends Response(200, body, Map("content-type" -> "text/markdown"))
+class OkResponse(body: String) extends Response(200, body)
 
-class UnprocessableEntityResponse(message: String)
-  extends Response(422, message, Map("content-type" -> "text/markdown"))
+class UnprocessableEntityResponse(message: String) extends Response(422, message)
 
-class InternalServerErrorResponse(message: String)
-  extends Response(500, message, Map("content-type" -> "text/markdown"))
+class InternalServerErrorResponse(message: String) extends Response(500, message)
 
-class BadGatewayResponse(message: String)
-  extends Response(500, message, Map("content-type" -> "text/markdown"))
+class BadGatewayResponse(message: String) extends Response(500, message)

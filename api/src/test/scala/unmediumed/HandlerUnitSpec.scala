7a22d7c8f8e2dcc6e403a9f009700371346ae07b
Daniel Ireson <daniel.ireson@zestia.com>
2018-08-03 23:35:27 +0100

    test just api gateway format in handler unit spec

--- api/src/test/scala/unmediumed/HandlerUnitSpec.scala
+++ api/src/test/scala/unmediumed/HandlerUnitSpec.scala
@@ -1,29 +1,13 @@
 package unmediumed
 
 import com.amazonaws.services.lambda.runtime.Context
-import org.mockito.Mockito._
-import org.mockito.ArgumentMatchers._
-import unmediumed.parse.{HtmlParser, MediumPost}
-import unmediumed.request.{Input, PathParser}
+import unmediumed.request.Input
 import unmediumed.response.Output
-import unmediumed.source.WebsiteScraper
 
 class HandlerUnitSpec extends TestHelpers {
-  private trait HandlerFixture {
-    val pathParser: PathParser = mock[PathParser]
-    doReturn("", "").when(pathParser).parse(any())
-
-    val websiteScraper: WebsiteScraper = mock[WebsiteScraper]
-    doReturn("", "").when(websiteScraper).scrape(anyString())
-
-    val htmlParser: HtmlParser = mock[HtmlParser]
-    val post = MediumPost(Map(), List())
-    doReturn(post, post).when(htmlParser).parse(anyString())
-  }
-
-  "Handler" should "return an api gateway formatted output" in new HandlerFixture {
+  "Handler" should "return an api gateway formatted output" in {
     // given
-    val testSubject = new Handler(pathParser, websiteScraper, htmlParser)
+    val testSubject = new Handler(null, null, null)
 
     val input = new Input
     input.setHttpMethod("GET")
@@ -36,34 +20,5 @@ class HandlerUnitSpec extends TestHelpers {
     output.getStatusCode shouldBe a[Integer]
     output.getHeaders shouldBe a[java.util.Map[_, _]]
     output.getBody shouldBe a[String]
-  }
-
-  it should "return an internal server error response when request is null" in new HandlerFixture {
-    // given
-    val testSubject = new Handler(pathParser, websiteScraper, htmlParser)
-
-    val input: Input = null
-
-    // when
-    val output: Output = testSubject.handleRequest(input, mock[Context])
-
-    output.statusCode shouldBe 500
-    output.headers.get("content-type") shouldBe "text/markdown; charset=utf-8"
-  }
-
-  it should "return a ok response when request is valid" in new HandlerFixture {
-    // given
-    val testSubject = new Handler(pathParser, websiteScraper, htmlParser)
-
-    val input = new Input
-    input.setHttpMethod("GET")
-    input.setPath("/@author/title")
-
-    // when
-    val output: Output = testSubject.handleRequest(input, mock[Context])
-
-    // then
-    output.statusCode shouldBe 200
-    output.headers.get("content-type") shouldBe "text/markdown; charset=utf-8"
   }
 }

7824cc91b1314c5a18507ba516c3a465d35b928d
Daniel Ireson <daniel.ireson@zestia.com>
2018-08-04 17:09:16 +0100

    add handler unit tests for responses

--- api/src/test/scala/unmediumed/HandlerUnitSpec.scala
+++ api/src/test/scala/unmediumed/HandlerUnitSpec.scala
@@ -1,13 +1,73 @@
 package unmediumed
 
 import com.amazonaws.services.lambda.runtime.Context
-import unmediumed.request.Input
+import org.mockito.Mockito.{doReturn, doThrow}
+import unmediumed.parse.{HtmlParseFailedException, HtmlParser, MediumPost}
+import unmediumed.request.{Input, PathParseFailedException, PathParser}
 import unmediumed.response.Output
+import unmediumed.source.{WebsiteScrapeFailedException, WebsiteScraper}
 
 class HandlerUnitSpec extends TestHelpers {
   "Handler" should "return an api gateway formatted output" in {
     // given
-    val testSubject = new Handler(null, null, null)
+    val pathParser = mock[PathParser]
+    doReturn("http://example.com", "http://example.com").when(pathParser).parse("/http://example.com")
+
+    val websiteScraper = mock[WebsiteScraper]
+    doReturn("", "").when(websiteScraper).scrape("http://example.com")
+
+    val htmlParser = mock[HtmlParser]
+    val post = MediumPost(Map(), List())
+    doReturn(post, post).when(htmlParser).parse("")
+
+    val testSubject = new Handler(pathParser, websiteScraper, htmlParser)
+
+    val input = new Input
+    input.setHttpMethod("GET")
+    input.setPath("/http://example.com")
+
+    // when
+    val output: Output = testSubject.handleRequest(input, mock[Context])
+
+    // then
+    output.getStatusCode shouldBe a[Integer]
+    output.getHeaders shouldBe a[java.util.Map[_, _]]
+    output.getBody shouldBe a[String]
+  }
+
+  it should "return an ok response" in {
+    // given
+    val pathParser = mock[PathParser]
+    doReturn("http://example.com", "http://example.com").when(pathParser).parse("/http://example.com")
+
+    val websiteScraper = mock[WebsiteScraper]
+    doReturn("", "").when(websiteScraper).scrape("http://example.com")
+
+    val htmlParser = mock[HtmlParser]
+    val post = MediumPost(Map(), List())
+    doReturn(post, post).when(htmlParser).parse("")
+
+    val testSubject = new Handler(pathParser, websiteScraper, htmlParser)
+
+    val input = new Input
+    input.setHttpMethod("GET")
+    input.setPath("/http://example.com")
+
+    // when
+    val output: Output = testSubject.handleRequest(input, mock[Context])
+
+    // then
+    output.getStatusCode shouldBe 200
+    output.headers.get("content-type") shouldBe "text/markdown; charset=utf-8"
+  }
+
+  it should "map a parse failed exception to an unprocessable entity response" in {
+    // given
+    val pathParser = mock[PathParser]
+    val exception = new PathParseFailedException("testing")
+    doThrow(exception, exception).when(pathParser).parse("/")
+
+    val testSubject = new Handler(pathParser, mock[WebsiteScraper], mock[HtmlParser])
 
     val input = new Input
     input.setHttpMethod("GET")
@@ -17,8 +77,69 @@ class HandlerUnitSpec extends TestHelpers {
     val output: Output = testSubject.handleRequest(input, mock[Context])
 
     // then
-    output.getStatusCode shouldBe a[Integer]
-    output.getHeaders shouldBe a[java.util.Map[_, _]]
-    output.getBody shouldBe a[String]
+    output.getStatusCode shouldBe 422
+    output.getBody shouldBe "Please provide a valid Medium URL or path"
+  }
+
+  it should "map a website scrape failed exception to a bad gateway response" in {
+    // given
+    val pathParser = mock[PathParser]
+    doReturn("http://example.com", "http://example.com").when(pathParser).parse("/http://example.com")
+
+    val websiteScraper = mock[WebsiteScraper]
+    val exception = new WebsiteScrapeFailedException("testing")
+    doThrow(exception, exception).when(websiteScraper).scrape("http://example.com")
+
+    val testSubject = new Handler(pathParser, websiteScraper, mock[HtmlParser])
+
+    val input = new Input
+    input.setHttpMethod("GET")
+    input.setPath("/http://example.com")
+
+    // when
+    val output: Output = testSubject.handleRequest(input, mock[Context])
+
+    // then
+    output.getStatusCode shouldBe 502
+    output.getBody shouldBe "Unable to fetch Medium post"
+  }
+
+  it should "map a html parse failed exception to an internal server error response" in {
+    // given
+    val pathParser = mock[PathParser]
+    doReturn("http://example.com", "http://example.com").when(pathParser).parse("/http://example.com")
+
+    val websiteScraper = mock[WebsiteScraper]
+    doReturn("", "").when(websiteScraper).scrape("http://example.com")
+
+    val htmlParser = mock[HtmlParser]
+    val exception = new HtmlParseFailedException("testing")
+    doThrow(exception, exception).when(htmlParser).parse("")
+
+    val testSubject = new Handler(pathParser, websiteScraper, htmlParser)
+
+    val input = new Input
+    input.setHttpMethod("GET")
+    input.setPath("/http://example.com")
+
+    // when
+    val output: Output = testSubject.handleRequest(input, mock[Context])
+
+    // then
+    output.getStatusCode shouldBe 500
+    output.getBody shouldBe "Unable to parse Medium post"
+  }
+
+  it should "return an internal server error response for non checked exceptions" in {
+    // given
+    val testSubject = new Handler(mock[PathParser], mock[WebsiteScraper], mock[HtmlParser])
+    val input = null
+
+    // when
+    val output: Output = testSubject.handleRequest(input, mock[Context])
+
+    // then
+    output.getStatusCode shouldBe 500
+    output.getBody shouldBe "An unexpected error occurred"
   }
 }
